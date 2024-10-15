7510b86dead8afd64e82ad043cb84942a7b15aac
Prihasta <prihasta2004@gmail.com>
2024-10-15 22:42:30 +0700

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active'); 
 });
+
+const videos = document.querySelectorAll('.card-video');
+
+videos.forEach(video => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  });
+  video.addEventListener('mouseout', () => {
+    video.pause();
+    video.currentTime = 0;  // Reset video to the start when hover ends
+  });
+});
