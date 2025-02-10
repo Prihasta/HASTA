51f2b06e0c58508c48cfc559d17c69f9490f4b8d
Prihasta <prihasta2004@gmail.com>
2025-02-10 15:18:33 +0700

    fixes

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,12 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active'); 
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const videos = document.querySelectorAll(".grid-item video");
     const overlay = document.getElementById("fullscreenOverlay");
